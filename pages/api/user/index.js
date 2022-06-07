49febe1e4156a60e6b5f72f52c9b45ac9f369c73
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-06-07 13:24:36 +0530

    Connect moved outside

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -1,21 +1,24 @@
-import connectMongo from '../../../database/connection';
-import UserDB from '../../../database/Schemas/user';
-import withProtect from '../../../middlewares/withProtect'
+import connectMongo from "../../../database/connection";
+import UserDB from "../../../database/Schemas/user";
+import withProtect from "../../../middlewares/withProtect";
 /**
  * @param {import('next').NextApiRequest} req
  * @param {import('next').NextApiResponse} res
  */
 async function createUserHandler(req, res) {
   try {
-    console.log('CONNECTING TO MONGO');
-    await connectMongo();
-    console.log('CONNECTED TO MONGO');
+    // console.log('CONNECTING TO MONGO');
+    // await connectMongo();
+    // console.log('CONNECTED TO MONGO');
 
-    console.log('CREATING DOCUMENT');
-    const user=new UserDB({firstName:"Sita",lastName:"Ram",mobile:"9876545766"})
-  // res.json({ user: req.user });
-  res.json({user})
-
+    // console.log('CREATING DOCUMENT');
+    const user = new UserDB({
+      firstName: "Sita",
+      lastName: "Ram",
+      mobile: "9876545766",
+    });
+    // res.json({ user: req.user });
+    res.json({ user });
   } catch (error) {
     console.log(error);
     res.json({ error });
