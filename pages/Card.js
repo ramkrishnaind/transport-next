d32473bd9398d8f7d65da340992b292f8c9a9a2e
saianita <50478345+saianita@users.noreply.github.com>
2022-06-10 15:57:10 +0530

    Card alignment adjust

--- pages/Card.js
+++ pages/Card.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Card = ({ image, item, itemCount }) => (
-  <div className="flex flex-row space-x-3 px-2 py-4 bg-white border-2 border-gray-100 rounded justify-between shadow-md">
-    <div><img src={image} alt=""/></div>
+  <div className="flex px-2 py-4 bg-white border-2 border-gray-100 rounded shadow-md">
+    <div className="mr-5"><img src={image} alt=""/></div>
     <div className="text-gray-500">{item}</div>
-    <button className="text-gray-500">{itemCount}</button>
+    <button className="text-gray-500 ml-auto ">{itemCount}</button>
   </div>
 );
 
