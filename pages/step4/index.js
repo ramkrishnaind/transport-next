615cf9268862d5f76d059037d340910f6db94da7
saianita <50478345+saianita@users.noreply.github.com>
2022-06-17 21:27:06 +0530

    JSON modified

--- pages/step4/index.js
+++ pages/step4/index.js
@@ -2,7 +2,36 @@ import React, { useEffect, useState } from "react";
 import ItemCard from "../ItemCard";
 import Card from "../Card";
 
-const Step4 = () => {
+const Step4 = (props) => {
+  const items = [
+    { name: "Sofasets", img: "images/sofa50.png" },
+    { name: "TVs", img: "images/tv50.png" },
+    { name: "Bikes", img: "images/bike50.png" },
+    { name: "Tables", img: "images/table50.png" },
+    { name: "Refrigerators", img: "images/fridge50.png" },
+    { name: "Cars", img: "images/cars50.png" },
+    { name: "Chairs", img: "images/chair50.png" },
+    { name: "Washing Machines", img: "images/washingmachine50.png" },
+    { name: "Cycles", img: "images/bicycle50.png" },
+    { name: "Cots", img: "images/bed50.png" },
+    { name: "Ovens", img: "images/microwave50.png" },
+    { name: "Mattress", img: "images/mattress50.png" },
+    { name: "Air Conditioners", img: "images/air-conditioner50.png" },
+    { name: "Cupboards", img: "images/cupboard50.png" },
+    { name: "Fans/Coolers", img: "images/ceiling-fan-off50.png" },
+  ];
+
+  const [state, setState] = useState('');
+  console.log(state);
+
+  const changeState = ()=>{
+          setState("Sofasets");
+   };
+
+  const handleClick = () => {
+   // setDisplay1("visible");
+  };
+
   return (
     <div>
       <div className="flex justify-end mr-5 mt-5 mb-2 space-x-5">
@@ -22,23 +51,37 @@ const Step4 = () => {
       <div className="flex justify-end mr-5  mb-5 text-sm ">
         <p>Do you know you can save this progress</p>
       </div>
-      <div className=" max-w-screen-xl m-auto py-10 mt-5 px-5 border">
-        <div className="grid space-x-1 lg:grid-cols-12">
-          <ItemCard image={"images/sofa50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/table50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/chair50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/bed50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/mattress50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/cupboard50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/tv50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/fridge50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/washingmachine50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/microwave50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/air-conditioner50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/ceiling-fan-off50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/bike50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/cars50.png"} itemCount={"0/1"} />
-          <ItemCard image={"images/bicycle50.png"} itemCount={"0/1"} />
+      <div className="flex overflow-x-auto space-x-4 w-1/2 m-auto py-2 mt-5 px-5 border">
+        <div className="flex flex-row space-x-4" onClick={handleClick}>
+          {items.map((element) => (
+            <div className="border px-2 ">
+              <div className="flex justify-center py-2">
+                <img src={element.img} alt="" />
+              </div>
+              <div className="text-center text-sm">{element.name}</div>
+              <div className="px-5 mt-2 hover:bg-blue-100">
+                <button className="text-gray-500 text-center m-auto" onClick={changeState}>
+                  0/1
+                </button>
+              </div>
+            </div>
+          ))}
+
+          {/* <div className="flex flex-row space-x-4">
+          {items.map((element) => (
+            <ItemCard
+              image={element.img}
+              itemCount={"0/1"}
+              name={element.name}
+            />
+          ))}
+        </div> */}
+
+          {/* <div className="flex flex-row space-x-4">
+          {items.map(element => (
+         <ItemCard image={element.img} itemCount={"0/1"} name={element.name} />
+        ))}
+        </div> */}
         </div>
       </div>
     </div>
