9687f5cfd6ce76385a9b05ce88da664d880d0e71
ravikharinta <ravikharinta2@gmail.com>
2022-06-27 16:01:13 +0530

    Customer find file run ok

--- pages/api/customer/findCustomerOrder.js
+++ pages/api/customer/findCustomerOrder.js
@@ -1,6 +1,6 @@
 // import connectMongo from "../../../database/connection";
 import dbConnect from "../../../database/lib/dbConnect";
-import UtilityItemDB from "../../../database/Schemas/utilityItem";
+import UtilityItemDB from "../../../database/Schemas/misItem";
 import BookingDB from "../../../database/Schemas/booking";
 import withProtect from "../../../middlewares/withProtect";
 const _ = require("lodash");
@@ -11,6 +11,12 @@ const booking_itemSchema = Joi.object({
     bookingId: Joi.objectId().required(),
     customerId: Joi.objectId().required(),
 });
+
+/**
+ * @param {import('next').NextApiRequest} req
+ * @param {import('next').NextApiResponse} res
+ */
+
 async function billnumfunc(req, res) {
   await dbConnect();
   try {
@@ -32,14 +38,16 @@ async function billnumfunc(req, res) {
 
     // pick data from req.body
     let booking_itemData = _.pick(req.body, ['bookingId','customerId']);
-    //let findData = await Cu stomerDB.findOne({ mobile: customerData.mobile });
+  
     let findData1 = await BookingDB.findOne({ customerId: booking_itemData.customerId });
-     let findData = await UtilityItemDB.findOne({customerId : booking_itemData.customerId });
+
+    let findData = await UtilityItemDB.findOne({customerId : booking_itemData.customerId });
+    
     if ((findData) || (findData1)) {
       return res.json({ status: true, error: false, message: "Data." + findData+findData1 })
     }
     else{
-        return res.json({ status: false, error: false, message: "Data not found." }) 
+      return res.json({ status: false, error: false, message: "Data not found." }) 
     }
   } catch (error) {
     console.log(error);
