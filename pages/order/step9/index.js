afa688eeb0b02c5e89e8963bd370179daf21c272
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-09-12 13:57:34 +0530

    build errors resolved

--- pages/order/step9/index.js
+++ pages/order/step9/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Card from "../Card";
+import Card from "../../Card";
 
 const Step9 = () => {
   const washingmachine = ["Washing Machine"];
--- pages/order/vehicle/index.js
+++ pages/order/vehicle/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Card from "../Card";
+import Card from "../../Card";
 
 const Vehicle = () => {
   const handleSubmit = async (event) => {
