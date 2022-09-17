6d13292ea850a048de4b0e95c76f745bcbd2e115
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-09-17 10:27:43 +0530

    Edit and tested save and edit

--- pages/order/step2/index.js
+++ pages/order/step2/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import data1 from "../../../data/bikeList.json";
 import Select from "react-select";
 import TransportContext from "../../../context";
-import {Button} from "antd";
+import { Button } from "antd";
 import { useRouter } from "next/router";
 import { liftAvailability } from "../../../services/customer-api-service";
 
@@ -50,6 +50,7 @@ const Step2 = () => {
   const handleSubmit = async (event) => {
     event.preventDefault(); // Prevent default submission
     let result = await callApi();
+    debugger;
     if (result.data.status) {
       console.log("liftAvailability result is", result);
       setBooking(result.data);
@@ -75,20 +76,23 @@ const Step2 = () => {
       isLiftAvailableOnMovingFloor: toLift.value,
     });
   };
+  debugger;
   const disabled = !(fromFloorType && fromLift && toFloorType && toLift);
   return (
-
     <>
       <div className="b1">
-
         <div className=" flex flex-row justify-between items-center p-0 gap-2.5 r1 h-16 r4 bg-white rounded-lg ">
-          <div className="pl-7 completepersentage not-italic font-semibold text-base flex-none order-none flex-grow-0 bg-white completing_bar_text">Set up 20% complete</div>
-          <div className="pr-7 not-italic font-semibold text-base flex-none order-none flex-grow-0 bg-white completing_bar_text">4 Step left: About 7 min</div>
+          <div className="pl-7 completepersentage not-italic font-semibold text-base flex-none order-none flex-grow-0 bg-white completing_bar_text">
+            Set up 20% complete
+          </div>
+          <div className="pr-7 not-italic font-semibold text-base flex-none order-none flex-grow-0 bg-white completing_bar_text">
+            4 Step left: About 7 min
+          </div>
         </div>
+      </div>
+      {/* DETAILS */}
 
-                                                                {/* DETAILS */}
-
-        <div className="flex flex-col items-left p-0 gap-2.5 r1 top-36 bg-white r4 rounded-lg pl-12 container ">
+      {/* <div className="flex flex-col items-left p-0 gap-2.5 r1 top-36 bg-white r4 rounded-lg pl-12 container ">
           <div className="bg-white text-9xl pl-10">
             <img
               className="stepimage"
@@ -99,114 +103,159 @@ const Step2 = () => {
           </div>
           <form>
             <div className="bg-white form_content">
-
-              <label htmlFor="name" className="bg-white w-20 mr-2 text-right font-bold text-gray-600 detailquestions">I currently live on</label>
-              <input 
+              <label
+                htmlFor="name"
+                className="bg-white w-20 mr-2 text-right font-bold text-gray-600 detailquestions"
+              >
+                I currently live on
+              </label>
+              <input
                 className="bg-white  border-b-2 border-gray-400  focuspt text-gray-600 placeholder-gray-400 outline-none w-28 detailfill pl-2"
                 type="text"
                 id="name"
                 name="name"
                 placeholder="Floor"
                 autoFocus
-                  required
-                
-              
+                required
               />
-              <label htmlFor="name" className="bg-white  w-20 mr-2 text-right font-bold  detailquestions text-gray-600">floor with service lift</label>
+              <label
+                htmlFor="name"
+                className="bg-white  w-20 mr-2 text-right font-bold  detailquestions text-gray-600"
+              >
+                floor with service lift
+              </label>
               <input
-                type="text" id="name" name="name" placeholder="Lift"
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Lift"
                 className="bg-white  border-b-2  border-gray-400 focuspt text-gray-600 placeholder-gray-400 outline-none detailfill w-96 pl-6"
               />
             </div>
 
-
             <div className="bg-white form_content">
+              <label
+                htmlFor="name"
+                className="bg-white  w-20 mr-2 text-right font-bold text-gray-600   detailquestions"
+              >
+                for shifting. I&apos; m moving to
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Floor"
+                className="bg-white  border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none w-96 detailfill pl-6"
+              />
 
-              <label htmlFor="name" className="bg-white  w-20 mr-2 text-right font-bold text-gray-600   detailquestions">for shifting. I&apos; m moving to</label>
-              <input type="text" id="name" name="name" placeholder="Floor"
-                className="bg-white  border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none w-96 detailfill pl-6" />
-
-              <label htmlFor="name" className="bg-white  w-20 mr-2 text-right font-bold text-gray-600 detailquestions">floor with service lift</label>
+              <label
+                htmlFor="name"
+                className="bg-white  w-20 mr-2 text-right font-bold text-gray-600 detailquestions"
+              >
+                floor with service lift
+              </label>
             </div>
-              <div className="bg-white">
-                <input type="text" id="name" name="name" placeholder="lift"
-                  className="bg-white  border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none w-24  detailfill pl-7" />
-                <label htmlFor="name" className="bg-white  w-20 mr-2 text-right font-bold text-gray-600 detailquestions">for shifting.</label>
-              </div>
+            <div className="bg-white">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="lift"
+                className="bg-white  border-b-2 border-gray-400 focus:border-green-400 text-gray-600 placeholder-gray-400 outline-none w-24  detailfill pl-7"
+              />
+              <label
+                htmlFor="name"
+                className="bg-white  w-20 mr-2 text-right font-bold text-gray-600 detailquestions"
+              >
+                for shifting.
+              </label>
+            </div> */}
 
-                                                                  {/* button */}
+      {/* button */}
 
-            <div className="bg-white ">
-              <Button className=" px-10 py-4 button_1 rounded-m "
-               loading={loading}
-               >
+      {/* <div className="bg-white ">
+              <Button
+                className=" px-10 py-4 button_1 rounded-m "
+                loading={loading}
+              >
                 Next
               </Button>
-            </div>
-
-          </form>
+            </div> */}
+      {/* </form>
         </div>
-      </div>
-    
-    <form className="max-w-screen-xl m-auto py-10 mt-10 px-5 border">
-      <div className="flex items-center mb-5 justify-center">
-        <label htmlFor="name" className="inline-block mr-2 text-gray-600">
-          I currently live on
-        </label>
-        <Select
-          className="block appearance w-40 bg-white  border-gray-400 hover:border-gray-500 px-4 py-2 focus:outline-none focus:shadow-outline"
-          defaultValue={fromFloorType}
-          onChange={setFromFloorType}
-          options={floorOptions}
-        />
+      </div>  */}
 
-        <label htmlFor="name" className="inline-block w-50 mr-1 text-gray-600">
-          floor with service lift
-        </label>
-        <Select
-          className="block appearance-none w-60 bg-white  border-gray-400 hover:border-gray-500 px-4 py-2 focus:outline-none focus:shadow-outline"
-          defaultValue={fromLift}
-          onChange={setFromLift}
-          options={liftOptions}
-        />
-        <label htmlFor="name" className="inline-block w-30 mr-1 text-gray-600">
-          for shifting.
-        </label>
-      </div>
-      <div className="flex items-center mb-5  justify-center">
-        <label htmlFor="name" className="inline-block w-30 mr-2 text-gray-600">
-          I&apos;m moving to
-        </label>
-        <Select
-          className="block appearance w-40 bg-white  border-gray-400 hover:border-gray-500 px-4 py-2 focus:outline-none focus:shadow-outline"
-          defaultValue={toFloorType}
-          onChange={setToFloorType}
-          options={floorOptions}
-        />
-        <label className="inline-block w-50 mr-1 text-gray-600 text-right">
-          floor with service lift
-        </label>
-        <Select
-          className="block appearance-none w-60 bg-white  border-gray-400 hover:border-gray-500 px-4 py-2 focus:outline-none focus:shadow-outline"
-          defaultValue={toLift}
-          onChange={setToLift}
-          options={liftOptions}
-        />
-        <label htmlFor="name" className="inline-block w-30 mr-1 text-gray-600">
-          for shifting.
-        </label>
-      </div>
-      <div className="flex justify-center items-center">
-        <button
-          className="mt-4 bg-blue-500 hover:bg-blue-400 text-green-100 border py-2 px-8 font-semibold text-lg rounded shadow-lg disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
-          type="submit"
-          onClick={handleSubmit}
-          disabled={disabled}
-        >
-          Next
-        </button>
-      </div>
-    </form>
+      <form className="max-w-screen-xl m-auto py-10 mt-10 px-5 border">
+        <div className="flex items-center mb-5 justify-center">
+          <label htmlFor="name" className="inline-block mr-2 text-gray-600">
+            I currently live on
+          </label>
+          <Select
+            className="block appearance w-40 bg-white  border-gray-400 hover:border-gray-500 px-4 py-2 focus:outline-none focus:shadow-outline"
+            defaultValue={fromFloorType}
+            onChange={setFromFloorType}
+            options={floorOptions}
+          />
+
+          <label
+            htmlFor="name"
+            className="inline-block w-50 mr-1 text-gray-600"
+          >
+            floor with service lift
+          </label>
+          <Select
+            className="block appearance-none w-60 bg-white  border-gray-400 hover:border-gray-500 px-4 py-2 focus:outline-none focus:shadow-outline"
+            defaultValue={fromLift}
+            onChange={setFromLift}
+            options={liftOptions}
+          />
+          <label
+            htmlFor="name"
+            className="inline-block w-30 mr-1 text-gray-600"
+          >
+            for shifting.
+          </label>
+        </div>
+        <div className="flex items-center mb-5  justify-center">
+          <label
+            htmlFor="name"
+            className="inline-block w-30 mr-2 text-gray-600"
+          >
+            I&apos;m moving to
+          </label>
+          <Select
+            className="block appearance w-40 bg-white  border-gray-400 hover:border-gray-500 px-4 py-2 focus:outline-none focus:shadow-outline"
+            defaultValue={toFloorType}
+            onChange={setToFloorType}
+            options={floorOptions}
+          />
+          <label className="inline-block w-50 mr-1 text-gray-600 text-right">
+            floor with service lift
+          </label>
+          <Select
+            className="block appearance-none w-60 bg-white  border-gray-400 hover:border-gray-500 px-4 py-2 focus:outline-none focus:shadow-outline"
+            defaultValue={toLift}
+            onChange={setToLift}
+            options={liftOptions}
+          />
+          <label
+            htmlFor="name"
+            className="inline-block w-30 mr-1 text-gray-600"
+          >
+            for shifting.
+          </label>
+        </div>
+        <div className="flex justify-center items-center">
+          <button
+            className="mt-4 bg-blue-500 hover:bg-blue-400 text-green-100 border py-2 px-8 font-semibold text-lg rounded shadow-lg disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none"
+            type="submit"
+            onClick={handleSubmit}
+            disabled={disabled}
+          >
+            Next
+          </button>
+        </div>
+      </form>
     </>
   );
 };
