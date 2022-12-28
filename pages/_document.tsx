2f915210af0918942c38e46b108867e487100a31
anjit pariyar <anjitpariyar@gmail.com>
2022-12-29 02:02:32 +0545

    build error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,16 @@
-import Document, { Html, Main, Head, NextScript } from "next/document";
+import Document, {
+  Html,
+  Main,
+  Head,
+  NextScript,
+  DocumentInitialProps,
+  DocumentContext,
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
-
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -16,12 +24,12 @@ export default class MyDocument extends Document {
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        styles: (
+        styles: [
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
-          </>
-        ),
+          </>,
+        ],
       };
     } finally {
       sheet.seal();
